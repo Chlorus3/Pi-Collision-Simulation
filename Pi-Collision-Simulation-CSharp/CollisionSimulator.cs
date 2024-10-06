bb8a3bdf9047cf565ddb59439ecc85c383d28985
Chlorus <99441357+Chlorus3@users.noreply.github.com>
2024-10-06 16:08:22 +0100

    Added code

---
+++ Pi-Collision-Simulation-CSharp/CollisionSimulator.cs
@@ -0,0 +1,51 @@
+﻿namespace Pi_Collision_Simulation_CSharp
+{
+	public class Cube(double _mass, double _velocity)
+	{
+		public double mass = _mass;
+		public double velocity = _velocity;
+	}
+
+	public class CollisionSimulator
+	{
+		public static (double, double) SimulateCollision(Cube cube1, Cube cube2)
+		{
+			// Simulates a perfect inelastic collision
+			return (
+				(cube1.mass - cube2.mass) /
+				(cube1.mass + cube2.mass) *
+				cube1.velocity + 2 * cube2.mass /
+				(cube1.mass + cube2.mass) *
+				cube2.velocity,
+
+				2 * cube1.mass /
+				(cube1.mass + cube2.mass) *
+				cube1.velocity + (cube2.mass - cube1.mass) /
+				(cube1.mass + cube2.mass) *
+				cube2.velocity
+			);
+		}
+
+
+		public static int RunSimulation(Cube cube1, Cube cube2)
+		{
+			int collisions = 0;
+
+			// Collision cannot happen if cube2 is equal to or greater than cube1
+			while (cube2.velocity < cube1.velocity)
+			{
+				(cube1.velocity, cube2.velocity) = SimulateCollision(cube1, cube2); collisions++;
+
+				/*
+				 * There can not be a collision with the wall if cube1 is not going towards the left
+				 * It must have a velocity less than 1
+				 */
+				if (cube1.velocity >= 0) break;
+
+				cube1.velocity *= -1; collisions++; // Collision with wall. The velocity of cube is multiplied by -1 to go towards the right
+			}
+
+			return collisions;
+		}
+	}
+}
--- Pi-Collision-Simulation-CSharp/Program.cs
+++ Pi-Collision-Simulation-CSharp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Pi_Collision_Simulation_CSharp;
+
+public class Program
+{
+	static readonly int DigitsOfPi = 5; // number of decimal places you want to calculate Pi to, must be >= 1
+
+	public static void Main()
+	{
+		Cube cube1 = new(1, 0);
+		Cube cube2 = new(Math.Pow(100, DigitsOfPi), -1);
+
+		int collisions = CollisionSimulator.RunSimulation(cube1, cube2);
+
+		Console.WriteLine(collisions / Math.Pow(10, DigitsOfPi));
+	}
+}
