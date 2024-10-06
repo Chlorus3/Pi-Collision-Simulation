7def202fb9a9a6e9fd9aea1d4a5893b2c8783b24
Chlorus <99441357+Chlorus3@users.noreply.github.com>
2024-10-06 16:31:46 +0100

    Renamed variable

--- Pi-Collision-Simulation-CSharp/Program.cs
+++ Pi-Collision-Simulation-CSharp/Program.cs
@@ -2,15 +2,15 @@
 
 public class Program
 {
-	static readonly int DigitsOfPi = 5; // number of decimal places you want to calculate Pi to, must be >= 1
+	static readonly int DecimalPlacesOfPi = 5; // number of decimal places you want to calculate Pi to, must be >= 1
 
 	public static void Main()
 	{
 		Cube cube1 = new(1, 0);
-		Cube cube2 = new(Math.Pow(100, DigitsOfPi), -1);
+		Cube cube2 = new(Math.Pow(100, DecimalPlacesOfPi), -1);
 
 		int collisions = CollisionSimulator.RunSimulation(cube1, cube2);
 
-		Console.WriteLine(collisions / Math.Pow(10, DigitsOfPi));
+		Console.WriteLine(collisions / Math.Pow(10, DecimalPlacesOfPi));
 	}
 }
